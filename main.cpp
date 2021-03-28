02ff67dd551da97e673db41f553d9d747918cfcc
AngelizDiaz <70964897+AngelizDiaz@users.noreply.github.com>
2021-03-27 22:54:17 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+Programa que imprima dirección y teléfono en la pantalla
+ 
+Preparado por: Angeliz Díaz Sierra
+Fecha: 27 de marzo de 2021
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Angeliz Diaz Sierra\n"
+        <<"Calle El Manantial #1234\n"
+        <<"Rincón, PR 00243\n\n"
+        <<"Tel: (787) 123-3765 \n\n"
+        <<"Press any key to continue";
+    
+    return 0;
+}
